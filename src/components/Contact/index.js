779ff2b67e4e699ce8b04064a011ba5eaca0b1cd
revi1106 <122621905+revi1106@users.noreply.github.com>
2026-05-29 04:18:11 +0530

    Update index.js

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -12,7 +12,6 @@ import { ToastContainer, toast } from "react-toastify";
 
 import "react-toastify/dist/ReactToastify.css";
 
-import * as LottiePlayer from "@lottiefiles/lottie-player";
 
 import { useState } from "react";
 
